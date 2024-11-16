b1ead45e056da58d1ed8737faa2f6dd848c8f4e0
asthas0 <asthas@email.sc.edu>
2024-11-16 18:21:16 -0500

    Fixed #2

--- languagelearningapp/src/main/java/module-info.java
+++ languagelearningapp/src/main/java/module-info.java
@@ -1,4 +1,6 @@
+/** 
 module com.languageapp {
+    
     requires javafx.controls;
     requires javafx.fxml;
     requires junit;
@@ -25,4 +27,26 @@ module com.languageapp {
 
     opens com.controllers to javafx.fxml;
     exports com.controllers;
+}
+*/
+
+
+
+module com.narration {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires json.simple;
+    requires jlayer;
+    requires software.amazon.awssdk.core;
+    requires software.amazon.awssdk.services.polly;
+    requires software.amazon.awssdk.regions;
+    requires software.amazon.awssdk.utils;
+    requires org.slf4j;
+    requires org.slf4j.simple;
+    requires software.amazon.awssdk.awscore;
+    requires software.amazon.eventstream;
+    requires com.google.gson;
+
+    opens com.narration to javafx.fxml;
+    exports com.narration;
 }
