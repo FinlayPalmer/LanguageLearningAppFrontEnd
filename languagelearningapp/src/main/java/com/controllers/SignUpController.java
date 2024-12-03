fcd9bb632ba8c7b9327141ee570590e05f0a76f0
mbotteon <matthewbotteon@gmail.com>
2024-12-03 16:06:54 -0500

    fixed #21

--- languagelearningapp/src/main/java/com/controllers/SignUpController.java
+++ languagelearningapp/src/main/java/com/controllers/SignUpController.java
@@ -3,16 +3,29 @@ package com.controllers;
 import java.io.IOException;
 
 import com.languageapp.App;
+import com.model.Account;
+import com.model.AccountList;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
 
 /**
- * @author Finlay Palmer, Astha Singh
+ * @author Finlay Palmer, Astha Singh, Matthew Botteon
  */
 
 public class SignUpController {
-
+    // Initialize the id names for the fields in the sign-up form
+    @FXML
+    private TextField firstNameField;
+    @FXML
+    private TextField lastNameField;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private TextField dobField;
+    @FXML
+    private TextField usernameField;
     @FXML
     private PasswordField passwordField;
 
@@ -25,10 +38,14 @@ public class SignUpController {
     private void switchToSection() throws IOException {
         App.setRoot("Section");
     }
+    
     @FXML
-    private void submitSignUp() {
-        String password = passwordField.getText(); 
-        System.out.println("Password: " + password);
+    private void submitSignUp() throws IOException {
+        String password = passwordField.getText();
+        String username = usernameField.getText();
+        Account account = new Account(firstNameField.getText(), lastNameField.getText(), emailField.getText(), dobField.getText(), usernameField.getText(), passwordField.getText());
+        AccountList.getInstance().addAccount(account);
+        System.out.println("Password: " + password + username);
+        App.setRoot("Section");
     }
 }
-
