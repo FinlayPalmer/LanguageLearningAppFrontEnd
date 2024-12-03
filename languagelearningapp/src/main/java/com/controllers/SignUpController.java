5cd80fec645dd87b8c35c398fb8b2fc10810686e
mbotteon <matthewbotteon@gmail.com>
2024-12-03 16:38:42 -0500

    fixed #22

--- languagelearningapp/src/main/java/com/controllers/SignUpController.java
+++ languagelearningapp/src/main/java/com/controllers/SignUpController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import com.languageapp.App;
 import com.model.Account;
 import com.model.AccountList;
+import com.model.LanguageAppFacade;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
@@ -15,6 +16,7 @@ import javafx.scene.control.PasswordField;
  */
 
 public class SignUpController {
+
     // Initialize the id names for the fields in the sign-up form
     @FXML
     private TextField firstNameField;
@@ -39,13 +41,16 @@ public class SignUpController {
         App.setRoot("Section");
     }
     
+    // !!! This is commented out until the strings can be properly checked against the AccountList - matt
     @FXML
     private void submitSignUp() throws IOException {
-        String password = passwordField.getText();
-        String username = usernameField.getText();
-        Account account = new Account(firstNameField.getText(), lastNameField.getText(), emailField.getText(), dobField.getText(), usernameField.getText(), passwordField.getText());
-        AccountList.getInstance().addAccount(account);
-        System.out.println("Password: " + password + username);
-        App.setRoot("Section");
+        /*
+        // Sends the data from the form to the signUp method in Facade; if successful, the user is advanced to the next screen
+        if (LanguageAppFacade.getInstance().signUp(firstNameField.getText(), lastNameField.getText(), 
+                                                   emailField.getText(), dobField.getText(), usernameField.getText(),
+                                                   passwordField.getText()).startsWith("Success"))
+            App.setRoot("Section");
+        // If unsuccessful, the user is sent to a sign up screen displaying an error message
+        else */ App.setRoot("SignUpFailure");
     }
 }
