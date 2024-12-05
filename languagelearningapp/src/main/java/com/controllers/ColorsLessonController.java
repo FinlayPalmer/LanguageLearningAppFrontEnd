0fe2425c4770e0f20284f3a742a358ef640b4d8b
mbotteon <matthewbotteon@gmail.com>
2024-12-05 14:23:45 -0500

    fixed #40

---
+++ languagelearningapp/src/main/java/com/controllers/ColorsLessonController.java
@@ -0,0 +1,65 @@
+package com.controllers;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+
+import com.languageapp.App;
+
+/**
+ * @author Matthew Botteon
+ */
+public class ColorsLessonController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("StartScreen");
+    }
+
+    @FXML
+    private void switchToEasy() throws IOException {
+        App.setRoot("EasyLessons");
+    }
+
+    @FXML
+    private void switchToColors() throws IOException {
+        App.setRoot("ColorsLesson");
+    }
+
+    @FXML
+    private void switchToFamily() throws IOException {
+        App.setRoot("FamilyLesson");
+    }
+
+    @FXML
+    private void switchToGreetings() throws IOException {
+        App.setRoot("GreetingsLesson");
+    }
+
+    @FXML
+    private void switchToLocked() throws IOException {
+        App.setRoot("LockedLesson");
+    }
+
+    @FXML
+    private void switchToProfile() throws IOException {
+        App.setRoot("Profile");
+    }
+
+    @FXML
+    private void switchToSection() throws IOException {
+        App.setRoot("Section");
+    }
+
+    @FXML
+    private void switchToFlashcard() throws IOException {
+        App.setRoot("Flashcard");
+    }
+
+    // This should point to the current lesson not always colors!!! - matt
+    @FXML
+    private void switchToCurrentLesson() throws IOException {
+        App.setRoot("ColorsLesson");
+    }
+
+}
