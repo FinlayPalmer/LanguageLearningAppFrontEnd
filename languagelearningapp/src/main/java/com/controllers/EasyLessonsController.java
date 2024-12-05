bc6310afb50242590f43cda3c142077d71484cec
mbotteon <matthewbotteon@gmail.com>
2024-12-05 13:08:14 -0500

    fixed #35

--- languagelearningapp/src/main/java/com/controllers/EasyLessonsController.java
+++ languagelearningapp/src/main/java/com/controllers/EasyLessonsController.java
@@ -50,6 +50,11 @@ public class EasyLessonsController {
         App.setRoot("Section");
     }
 
+    @FXML
+    private void switchToFlashcard() throws IOException {
+        App.setRoot("FlashcardQuestion");
+    }
+
     // This should point to the current lesson not always colors!!! - matt
     @FXML
     private void switchToCurrentLesson() throws IOException {
